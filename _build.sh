9a762109575434979b50cafa4f9b209405587c51
jensengrey <jensengrey@localhost>
2023-02-02 20:05:50 +0000

    enhanced build, better cross platform

--- _build.sh
+++ _build.sh
@@ -3,6 +3,17 @@
 set -eux;
 
 # apt install libboost-dev -y
-g++ -std=c++20 -g -O2 -fno-omit-frame-pointer -fno-inline hello-crow.cpp -lpthread -o hello-crow
+if [ "$(uname)" == "Darwin" ]; then
+    g++ -std=c++20 -g -O2 -fno-omit-frame-pointer -fno-inline hello-crow.cpp -lpthread -I/opt/homebrew/include -o hello-crow
+elif [ "$(uname)" == "Linux" ]; then
+    g++ -std=c++20 -g -O2 -fno-omit-frame-pointer -fno-inline hello-crow.cpp -lpthread -o hello-crow
+else
+    echo "trying the linux build on: $(uname), might work"
+    g++ -std=c++20 -g -O2 -fno-omit-frame-pointer -fno-inline hello-crow.cpp -lpthread -o hello-crow
+fi
 
-docker build -t hello-crow -f crow.df .
+if command -v docker >/dev/null 2>&1; then
+    docker build -t hello-crow -f crow.df .
+fi
+
+
